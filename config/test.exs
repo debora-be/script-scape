8d8c30ed227e07f79822e58981aada36acd627b3
debora berte <arobedbee@gmail.com>
2024-02-25 16:18:31 -0300

    Config: app to connect with database through compose network

--- config/test.exs
+++ config/test.exs
@@ -6,9 +6,9 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :script_scape, ScriptScape.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: "script_scape_access",
+  password: "script_scape_pass",
+  hostname: "script_scape_db",
   database: "script_scape_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
@@ -16,7 +16,7 @@ config :script_scape, ScriptScape.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :script_scape, ScriptScapeWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [ip: {0, 0, 0, 0}, port: 4002],
   secret_key_base: "nZ3/lSWR2Yd/985s+1onvzlzhhV0P6P1uo4qLF7lzmuky+3kVGcAk1h7QG2xwx/A",
   server: false
 
